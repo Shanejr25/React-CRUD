f4ca1738dce2aa0418ab5fe169fe6c8b33970258
Shanejr <stlazjr0@gmail.com>
2020-07-23 03:20:37 -0700

    added products and displayed them, started delete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
+addimport React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const products = [
+  {
+    name: 'iPad',
+    price: 250
+  },
+  {
+    name: 'iPhone',
+    price: 650
+  }
+];
+
+localStorage.setItem('products', JSON.stringify(products)); // setup local storage
+class App extends Component {
+  // constructor method
+  constructor(props) {
+    super(props);
+    
+    // manage the state
+    this.state = {
+      products: []
+    }
+  }
+  componentWillMount(){ // check what data WILL be grabbed from the component
+    // need javascript version
+    // const products = JSON.parse(localStorage.getItem('products'));
+    //const products = localStorage.getItem('products'); // get the products with json stringify version
+    // call the getproducts function 
+    this.getProducts(); 
+
+    // console.log(products); // check that you are getting the products
+  }
+
+  getProducts() {
+    const products = JSON.parse(localStorage.getItem('products'));
+
+    this.setState({
+      products
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Products Manager</h1>
+        {
+          this.state.products.map(products => { // key of products and displace spans with name and price
+            return (
+              <div key={products.name}>
+                <span>{products.name}</span> 
+                {` | `} 
+                <span>{products.price}</span>
+                {` | `} 
+                <button>Delete</button>
+              </div>
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
