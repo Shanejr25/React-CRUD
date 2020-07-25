b98d0df295c484fa21ba72379cc69b125dd1502c
unknown <stlazjr0@gmail.com>
2020-07-24 19:09:32 -0700

    forgot stuff

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ProductItem from './ProductItem'
-import AddProduct from './AddItem'
+import AddProduct from './AddProduct'
 
 const products = [
   {
